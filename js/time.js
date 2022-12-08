8a6a4b0441be6c38ce0eebf7ff4f6036bcd1a969
xiaoclover <996692911.com>
2022-12-08 21:37:43 +0800

    github action update

---
+++ js/time.js
@@ -0,0 +1,17 @@
+function show_runtime() {
+  window.setTimeout('show_runtime()', 1000)
+  X = new Date('9/26/2022 10:28:00')
+  Y = new Date()
+  T = Y.getTime() - X.getTime()
+  M = 24 * 60 * 60 * 1000
+  a = T / M
+  A = Math.floor(a)
+  b = (a - A) * 24
+  B = Math.floor(b)
+  c = (b - B) * 60
+  C = Math.floor((b - B) * 60)
+  D = Math.floor((c - C) * 60)
+  document.getElementById('runtime_span').innerHTML =
+    '👨‍🌾 感谢陪伴  ' + A + '天' + B + '小时' + C + '分' + D + '秒'
+}
+show_runtime()
